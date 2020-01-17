f711c6f39e50b496270bdfb6906191ae5433d007
Stefan Eriksson <stefan.k.j.eriksson@gmail.com>
2020-01-17 16:10:30 +0100

    Added TextToIP converter.

--- Wizard/Misc/Converters.cs
+++ Wizard/Misc/Converters.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.Text;
 using System.Windows.Data;
-using System.Net.Sockets;
+using System.Net;
 
 namespace Wizard.Misc
 {
@@ -17,6 +17,29 @@ namespace Wizard.Misc
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
             return !(bool)value;
+        }
+    }
+
+    public class TextToIpAddress : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var ip = value as IPAddress;
+            return ip.ToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var str = value.ToString();
+            IPAddress ip;
+            if (IPAddress.TryParse(str, out ip))
+            {
+                return ip;
+            }
+            else
+            {
+                return str;
+            }
         }
     }
 }
