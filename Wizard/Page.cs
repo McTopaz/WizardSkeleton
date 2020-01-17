64e6cbc6a2b75b0d469bfcd61c803cde49278bb2
Stefan Eriksson <stefan.k.j.eriksson@gmail.com>
2020-01-17 16:09:50 +0100

    Changed the dispose implement. Added in vmPageBase instead.

--- Wizard/Page.cs
+++ Wizard/Page.cs
@@ -8,7 +8,7 @@ using Wizard.Views.Pages;
 
 namespace Wizard
 {
-    class Page : IPage, IDisposable
+    class Page : IPage
     {
         public UserControl Content { get; set; }
         public Page Next { get; set; }
@@ -31,47 +31,18 @@ namespace Wizard
 
         public virtual void Opening()
         {
+            ViewModel.Opening();
         }
 
         public virtual void Closing()
         {
+            ViewModel.Closing();
         }
 
-        #region IDisposable Support
-        private bool disposedValue = false; // To detect redundant calls
-
-        protected virtual void Dispose(bool disposing)
-        {
-            if (!disposedValue)
-            {
-                if (disposing)
-                {
-                    // TODO: dispose managed state (managed objects).
-                }
-
-                // TODO: free unmanaged resources (unmanaged objects) and override a finalizer below.
-                // TODO: set large fields to null.
-
-                disposedValue = true;
-            }
-        }
-
-        // TODO: override a finalizer only if Dispose(bool disposing) above has code to free unmanaged resources.
-        // ~Page()
-        // {
-        //   // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
-        //   Dispose(false);
-        // }
-
-        // This code added to correctly implement the disposable pattern.
         public void Dispose()
         {
-            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
-            Dispose(true);
-            // TODO: uncomment the following line if the finalizer is overridden above.
-            // GC.SuppressFinalize(this);
+            ViewModel.Dispose();
         }
-        #endregion
     }
 
     class NoPage : Page
---
+++ Wizard/ViewModels/vmPageBase.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Controls;
+
+using Wizard.Misc;
+using Wizard.Views.Pages;
+
+namespace Wizard
+{
+    abstract class vmPageBase : IDisposable
+    {
+        public Title Title { get; protected set; } = new Title();
+        public UserControl Next { get; protected set; }
+        public IPage Container { get; set; }
+
+        public virtual void Opening()
+        {
+        }
+
+        public virtual void Closing()
+        {
+        }
+
+        #region IDisposable Support
+        private bool disposedValue = false; // To detect redundant calls
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: dispose managed state (managed objects).
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override a finalizer below.
+                // TODO: set large fields to null.
+
+                disposedValue = true;
+            }
+        }
+
+        // TODO: override a finalizer only if Dispose(bool disposing) above has code to free unmanaged resources.
+        // ~vmPageBase()
+        // {
+        //   // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+        //   Dispose(false);
+        // }
+
+        // This code added to correctly implement the disposable pattern.
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+            Dispose(true);
+            // TODO: uncomment the following line if the finalizer is overridden above.
+            // GC.SuppressFinalize(this);
+        }
+        #endregion
+    }
+}
